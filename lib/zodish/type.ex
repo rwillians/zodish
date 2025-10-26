eaf6a2a0c78856720f3bf0fa41ce345202893894
Rafael Willians <me@rwillians.com>
2025-10-26 11:46:06 -0300

    Updated docs

--- lib/zodish/type.ex
+++ lib/zodish/type.ex
@@ -1,14 +1,14 @@
 defprotocol Zodish.Type do
   @moduledoc ~S"""
-  Protocol for parsing values based on Zodish types.
+  Protocol for parsing values based on Zodish schemas.
   """
 
   @doc ~S"""
-  Parses a value based on the given Zodish type.
+  Parses a value based on the given Zodish schema.
   """
-  @spec parse(type :: t(), value :: any()) ::
+  @spec parse(schema :: t(), value :: any()) ::
           {:ok, any()}
           | {:error, Zodish.Issue.t()}
 
-  def parse(type, value)
+  def parse(schema, value)
 end
