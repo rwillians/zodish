f3d7f84ecd7e5e5b823c130814612ffff63c5df2
Rafael Willians <me@rwillians.com>
2025-10-26 11:48:48 -0300

    Set the default behavior to warn when coercing a type that doesn't support coercion

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,4 @@ import Config
 config :zodish,
   #   defines the expected behavior when trying to coerce a type that
   # ↓ doesn't support coercion, options are :raise, :warn or :ignore
-  on_unsupported_coercion: :raise
+  on_unsupported_coercion: :warn
